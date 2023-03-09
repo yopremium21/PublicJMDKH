9ce7be1f35101895a051637de5e21c5e890fc9da
yopremium21 <121346873+yopremium21@users.noreply.github.com>
2023-03-09 20:41:11 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,8 @@
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, execl as osexecl
+from os import path as ospath, environ
 from subprocess import run as srun
-from requests import get as rget
 from dotenv import load_dotenv
-from sys import executable
+from pymongo import MongoClient
 
 if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
@@ -13,55 +12,56 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+load_dotenv('config.env', override=True)
+
 try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
+    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
+        log_error('The README.md file there to be read! Exiting now!')
+        exit()
 except:
     pass
 
-load_dotenv('config.env', override=True)
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+if len(BOT_TOKEN) == 0:
+    log_error("BOT_TOKEN variable is missing! Exiting now")
+    exit(1)
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
-try:
-    if len(UPSTREAM_REPO) == 0:
-       raise TypeError
-except:
-    UPSTREAM_REPO = "https://github.com/weebzone/WZML"
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except:
-    UPSTREAM_BRANCH = 'update'
+bot_id = BOT_TOKEN.split(':', 1)[0]
 
-if ospath.exists('.git'):
-    srun(["rm", "-rf", ".git"])
+DATABASE_URL = environ.get('DATABASE_URL', '')
+if len(DATABASE_URL) == 0:
+    DATABASE_URL = None
 
-update = srun([f"git init -q \
-                 && git config --global user.email doc.adhikari@gmail.com \
-                 && git config --global user.name Karan \
-                 && git add . \
-                 && git commit -sm update -q \
-                 && git remote add origin {UPSTREAM_REPO} \
-                 && git fetch origin -q \
-                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+if DATABASE_URL:
+    conn = MongoClient(DATABASE_URL)
+    db = conn.mltb
+    if config_dict := db.settings.config.find_one({'_id': bot_id}):  #retrun config dict (all env vars)
+        environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
+        environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
+    conn.close()
 
-if update.returncode == 0:
-    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
-    log_info(f'Upstream Repo: {UPSTREAM_REPO}')
-    log_info(f'Upstream Branch: {UPSTREAM_BRANCH}')
-else:
-    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
-    log_info(f'Entered Upstream Repo: {UPSTREAM_REPO}')
-    log_info(f'Entered Upstream Branch: {UPSTREAM_BRANCH}')
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
+if len(UPSTREAM_REPO) == 0:
+   UPSTREAM_REPO = None
 
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
+if len(UPSTREAM_BRANCH) == 0:
+    UPSTREAM_BRANCH = 'master'
+
+if UPSTREAM_REPO:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
+
+    update = srun([f"git init -q \
+                     && git config --global user.email yopremium21@gmail.com \
+                     && git config --global user.name pablo \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+    if update.returncode == 0:
+        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    else:
+        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
